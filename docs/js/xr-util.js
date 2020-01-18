f331337659a7e16f098a5d61cb3af46f616ab90c
teracy55 <literacy1945@gmail.com>
2020-01-18 12:41:49 +0900

    initial commit

---
+++ docs/js/xr-util.js
@@ -0,0 +1,59 @@
+
+const XR_MODE_INLINE = 'inline';
+const XR_MODE_VR = 'immersive-vr';
+const XR_MODE_AR = 'immersive-ar';
+
+class XrUtil {
+    constructor() {
+        this.support = { inline: false, vr: false, ar: false };
+    }
+
+    async init() {
+        const result = await Promise.all([
+            this.isSupported(XR_MODE_INLINE),
+            this.isSupported(XR_MODE_VR),
+            this.isSupported(XR_MODE_AR),
+        ]);
+
+        this.support.inline = result[0];
+        this.support.vr = result[1];
+        this.support.ar = result[2];
+    }
+
+    async isSupported(mode) {
+        try {
+            return await navigator.xr.isSessionSupported(mode);
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+    }
+
+    async start(mode) {
+        try {
+            this.session = await navigator.xr.requestSession(mode);
+            return true;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+    }
+
+    async end() {
+        if (this.session) {
+            this.session.end();
+            this.session = null;
+        }
+    }
+
+    async play() {
+        try {
+            this.requestID = this.session.requestAnimationFrame((time, xrFrame) => {
+                console.log(time, xrFrame);
+            })
+        } catch (err) {
+            console.log(err);
+
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# WebXR Device API Sample
